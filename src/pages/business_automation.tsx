4c28ee02d6581d52c3f56ed4bd7a29ca0b98d3c3
Max-access89 <maxwell@access89.com>
2025-02-07 10:25:32 +0000

    update graphics

--- src/pages/business_automation.tsx
+++ src/pages/business_automation.tsx
@@ -1,9 +1,11 @@
-import InfoCard from '@/components/shared/info_card';
-import { CustomButton } from '@/components/shared/shared_customs';
-import { Icon } from '@iconify/react/dist/iconify.js';
-import { Image } from '@nextui-org/react';
+import InfoCard from "@/components/shared/info_card";
+import { CustomButton } from "@/components/shared/shared_customs";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { Image } from "@nextui-org/react";
+import { useNavigate } from "react-router-dom";
 
 const BusinessAutomation = () => {
+  const navigate = useNavigate();
   return (
     <main className="container">
       <section className="text-center pb-10">
@@ -14,11 +16,14 @@ const BusinessAutomation = () => {
           Point of Sale <br /> for wherever you sell
         </h1>
         <p className="text-secondary-black text-sm my-5">
-          From seamless logistics to on-time deliveries, we navigate the roads
-          to success with you
+          From efficient transactions to automated workflows, we streamline your
+          business operations for seamless growth.
         </p>
-        <CustomButton className="bg-primary text-white font-medium px-5 ">
-          Explore <Icon icon="solar:arrow-right-outline" fontSize={20} />
+        <CustomButton
+          onPress={() => navigate("/onboarding")}
+          className="bg-primary text-white font-medium px-5 "
+        >
+          Get Started <Icon icon="solar:arrow-right-outline" fontSize={20} />
         </CustomButton>
       </section>
 
@@ -26,20 +31,20 @@ const BusinessAutomation = () => {
         <div className="absolute -top-24 md:-top-10 left-1/2 transform z-50 -translate-x-1/2 w-[70%] lg:w-[60%] mx-auto bg-white rounded-3xl p-4 grid grid-cols-auto-fill-150 gap-2">
           {[
             {
-              title: 'Manage Cashflow',
-              description: 'Efficiently control and monitor finances',
+              title: "Manage Cashflow",
+              description: "Efficiently control and monitor finances",
             },
             {
-              title: 'Reach Clients',
-              description: 'Easily connect and engage customers',
+              title: "Reach Clients",
+              description: "Easily connect and engage customers",
             },
             {
-              title: 'Advanced Reporting',
-              description: 'Detailed insights and performance analysis',
+              title: "Advanced Reporting",
+              description: "Detailed insights and performance analysis",
             },
             {
-              title: 'Grow your business',
-              description: 'Fast and easy access to loans for expansion',
+              title: "Grow your business",
+              description: "Fast and easy access to loans for expansion",
             },
           ].map((item, index) => (
             <div key={index}>
@@ -61,14 +66,14 @@ const BusinessAutomation = () => {
       <section className="md:grid grid-cols-2 gap-x-5 mt-8 space-y-4 md:space-y-0">
         {[
           {
-            title: 'Inventory',
+            title: "Inventory",
             description:
-              'Receive inventory, manage stock, and create purchase orders from a single platform.',
+              "Receive inventory, manage stock, and create purchase orders from a single platform.",
           },
           {
-            title: 'Business Insights',
+            title: "Business Insights",
             description:
-              'Streamline the allocation of resources and time slots, optimizing bookings to maximize business insights and efficiency',
+              "Streamline the allocation of resources and time slots, optimizing bookings to maximize business insights and efficiency",
           },
         ].map((item, index) => (
           <InfoCard
@@ -105,33 +110,33 @@ export default BusinessAutomation;
 
 const moreConstant = [
   {
-    title: 'HR & Payroll',
+    title: "HR & Payroll",
     description:
-      'Simplify your  business operations by combining HR tasks into one system. This module manages employee records, payroll, benefits, and attendance, ensuring accurate calculations and compliance',
+      "Simplify your  business operations by combining HR tasks into one system. This module manages employee records, payroll, benefits, and attendance, ensuring accurate calculations and compliance",
   },
   {
-    title: 'Wholesale & Retail',
+    title: "Wholesale & Retail",
     description:
-      'Access an integrated business management solution with financial tracking, operational control, pricing management, inventory oversight, and cash flow monitoring. Enhance efficiency, financial health, and focus on business growth.',
+      "Access an integrated business management solution with financial tracking, operational control, pricing management, inventory oversight, and cash flow monitoring. Enhance efficiency, financial health, and focus on business growth.",
   },
   {
-    title: 'Manufacturing',
+    title: "Manufacturing",
     description:
-      'Enhance your manufacturing operations with a unified manufacturing management solution covering production control, inventory management, quality assurance, and supply chain oversight, optimizing efficiency and supporting business expansion',
+      "Enhance your manufacturing operations with a unified manufacturing management solution covering production control, inventory management, quality assurance, and supply chain oversight, optimizing efficiency and supporting business expansion",
   },
   {
-    title: 'Accounting',
+    title: "Accounting",
     description:
-      'Improve your financial oversight with this advanced tool providing real-time insights into budgeting, expense management, revenue tracking, and transaction reconciliation for informed decision-making.',
+      "Improve your financial oversight with this advanced tool providing real-time insights into budgeting, expense management, revenue tracking, and transaction reconciliation for informed decision-making.",
   },
   {
-    title: 'CRM',
+    title: "CRM",
     description:
-      'Organize and manage customer interactions and data, optimizing customer service, sales, and marketing efforts to enhance relationships and drive business growth.',
+      "Organize and manage customer interactions and data, optimizing customer service, sales, and marketing efforts to enhance relationships and drive business growth.",
   },
   {
-    title: 'Hospitality',
+    title: "Hospitality",
     description:
-      'Manage your hospitality business with our comprehensive solution that integrates guest information, administrative tasks, financial management, and a special guest portal into one easy system.',
+      "Manage your hospitality business with our comprehensive solution that integrates guest information, administrative tasks, financial management, and a special guest portal into one easy system.",
   },
 ];

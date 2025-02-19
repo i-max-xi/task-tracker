b6cf36a5df85d9d4eb478fe065b70b2c989ae5fc
Max-access89 <maxwell@access89.com>
2025-02-19 15:18:00 +0000

    add componets and primitive ui

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
+import TaskList from "@/components/shared/ui/TaskList";
+import TaskForm from "@/components/shared/ui/TaskForm";
 
-const Home = () => {
+const Home: React.FC = () => {
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <>
-      <motion.div
-        initial={{ opacity: 0, x: 50 }}
-        animate={{ opacity: 1, x: 0 }}
-        className="flex flex-col items-center justify-center w-full h-[100vh]"
+      <div className=" mx-auto bg-white w-full">
+        <h1 className="text-3xl font-bold text-center mb-4">Task Tracker</h1>
+
+        <TaskList />
+      </div>
+
+      <motion.button
+        onClick={() => setShowForm(true)}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="fixed bottom-8 right-8 bg-blue-500 text-white p-4 rounded-full shadow-lg hover:bg-blue-600"
       >
-        <h1>Task Tracker</h1>
-      </motion.div>
+        Add Task
+      </motion.button>
+
+      {showForm && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <TaskForm onClose={() => setShowForm(false)} />
+        </div>
+      )}
     </>
   );
 };
